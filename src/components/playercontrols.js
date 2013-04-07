a49a06da4ed1a2d1dae189b51d5bcf61787aaffd
Kev Zettler <kevzettler@gmail.com>
2013-04-07 16:43:49 -0700

    omg movin

--- src/components/playercontrols.js
+++ src/components/playercontrols.js
@@ -1,31 +1,22 @@
 Crafty.c("PlayerControls", {
 
   init: function() {
-
-    this.requires("Fourway, ViewportConstrain, MoveByCenter")
-    .attr({enteredviewport: true})
-    .fourway(4,[0])     // twoway movement for now
+    this.requires("Fourway, ViewportConstrain, SpriteAnimation")
+    .fourway(4)
     .bind("NewDirection", this.changeDirection);
+
+    this.animate('Walking',2,0,0);
+    this.bind("Moved", this.movingAnimation);
   },
 
   changeDirection: function(direction){
     console.log("changing direction");
-    this.animate('RunningRight',2,0,0);
-    this.animate('RunningLeft',2,0,0);
+    this.flip("X");
+  },
 
-    if (direction.x < 0) {
-      if (!this.isPlaying("RunningLeft"))
-        this.stop().animate("RunningLeft", 25, -1);
-    }
-
-    if (direction.x > 0) {
-      if (!this.isPlaying("RunningRight"))
-        this.stop().animate("RunningRight", 25, -1);
-    }
-
-    if(!direction.x){
-      this.stop().animate("standing",0,0,0).animate("standing",25,0);
-    }
+  movingAnimation: function(movedata){
+    console.log("moving");
+    this.animate('Walking',25,-1);
   }
 
 });
