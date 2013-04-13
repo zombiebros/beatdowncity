c45d0c275d5ef401ee25a5909f1a62406ed7e56d
Kev Zettler <kevzettler@gmail.com>
2013-04-13 12:43:25 -0700

    more config for mobile

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,10 @@
 // Initialize Crafty
 var global_state = {
   current_level: 0,
-  loaded_maps: [],
+  loaded_maps: []
 };
 
 $(function(){
-	Crafty.init(960,600).canvas.init();
+	Crafty.init().canvas.init();
 	Crafty.scene("main");
 });
