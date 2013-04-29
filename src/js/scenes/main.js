4cbff31db441dd7524b31c4d51239ed0c00cb28d
Kev Zettler <kevzettler@gmail.com>
2013-04-28 21:44:53 -0700

    omg soo cool

--- src/js/scenes/main.js
+++ src/js/scenes/main.js
@@ -19,9 +19,25 @@ Crafty.scene("main",(function() {
 	var scene = {
 
     renderOtherUsers: function(otherUsers){
+      console.log("rendering other users");
       for(var user_id in otherUsers.val()){
         if(user_id != Crafty.player_id){
-          var remoteUser = Firebase.child('users').child(user_id);
+          console.log("calling render user in loop for ", user_id, otherUsers.child(user_id));
+          this.renderUserSnapshot(otherUsers.child(user_id));
+        }
+      }
+    },
+
+    // renderUserSnapshot: function(snapshot){
+    //   console.log("rendering user snapshot", snapshot, Crafty.player_id);
+    //   if(snapshot.name() != Crafty.player_id){
+    //     this.renderUser(snapshot.val());
+    //   }
+    // },
+
+    renderUserSnapshot: function(snapshot){
+          console.log("in renderUser", snapshot);
+          var remoteUser = Firebase.child('users').child(snapshot.name());
           var player = Crafty.e("Player, Collision")
           .attr({
             w:140,
@@ -30,35 +46,46 @@ Crafty.scene("main",(function() {
           })
           .collision([60,200], [140,200], [110,0], [80,0]);
 
-          remoteUser.on('value', function(snapshot){
-            player.attr(snapshot.val());
-          });
-        }
-      }
+          /* if its the local player */
+          if(snapshot.name() == Crafty.player_id){
+            player.addComponent('PlayerControls');
+            player.bind('EnterFrame', function(){
+              remoteUser.set({
+                x: this.x,
+                y: this.y
+              });
+            });
+          }else{
+            remoteUser.on('value', function(snapshot){
+              player.attr(snapshot.val());
+            });
+          }
     },
 
 		init: function(){
-      mixpanel.track("New Game");
-
       var remote = Firebase.child('users').child(Crafty.player_id);
-			var player = Crafty.e("Player, PlayerControls, Collision")
-			.attr({
-        w:140,
-        h:138,
-				x:Crafty.viewport.width/2-50,
-				y: Crafty.viewport.height - 200,
-				z: 2
-			})
-			.collision([60,200], [140,200], [110,0], [80,0])
-      .bind('EnterFrame', function(){
-        remote.set({
-          x: this.x,
-          y: this.y
-        });
-      });
+			// var player = Crafty.e("Player, PlayerControls, Collision")
+			// .attr({
+   //      w:140,
+   //      h:138,
+			// 	x:Crafty.viewport.width/2-50,
+			// 	y: Crafty.viewport.height - 200,
+			// 	z: 2
+			// })
+			// .collision([60,200], [140,200], [110,0], [80,0])
+   //    .bind('EnterFrame', function(){
+   //      remote.set({
+   //        x: this.x,
+   //        y: this.y
+   //      });
+   //    });
 
-      Firebase.child('users').once("value", this.renderOtherUsers);
-      //Firebase.child('users').on("");
+      Firebase.child('users').once("value", $.proxy(this.renderOtherUsers, this));
+      Firebase.child('users').on("child_added", $.proxy(this.renderUserSnapshot, this));
+      remote.set({
+        x:Crafty.viewport.width/2-50,
+        y: Crafty.viewport.height - 200
+      });
 		}
 	};
 
